96320a4c498a859ce5ebc56c75a3be4dc35e866c
aofilan <aofilan@ucsc.edu>
2024-02-29 19:46:22 -0800

    fixed and imported tables

--- backend/data_tables/create_tables.sql
+++ backend/data_tables/create_tables.sql
@@ -1,23 +1,23 @@
- CREATE TABLE account (
+ CREATE TABLE accountinfo (
     userid INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     firstname VARCHAR(100) NOT NULL,
     lastname VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    passcode VARCHAR(100) NOT NULL
  );
 
  CREATE TABLE projects(
     projectid INT AUTO_INCREMENT PRIMARY KEY, 
     userid INT NOT NULL, 
-    sortedID INT AUTO_INCREMENT NOT NULL,
-    sorted VARBINARY(MAX) NOT NULL,
-    FOREIGN KEY (userid) REFERENCES account
+    sortedID INT NOT NULL,
+    sorted LONGBLOB NOT NULL,
+    FOREIGN KEY (userid) REFERENCES accountinfo(userid)
  );
 
  CREATE TABLE files(
     fileid INT AUTO_INCREMENT PRIMARY KEY,
     projectid INT NOT NULL,
-    filedata VARBINARY(MAX) NOT NULL,
-    FOREIGN KEY (projectid) REFERENCES projects
+    filedata LONGBLOB NOT NULL,
+    FOREIGN KEY (projectid) REFERENCES projects(projectid)
  );
