560a748d3813139ff1dce5808a1f83c2eab41104
Phuong <phuongunguyen4@gmail.com>
2024-03-12 15:52:00 -0700

    added more buttons, styled a page, attempted to do endpoints

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import { Switch } from "./Switch.js";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from "./components/Theme.js";
 
 // adding the routes to the app
 
 function App() {
     return (
-        <Switch />
+        <ThemeProvider theme={theme}>
+            <Switch />
+        </ThemeProvider>
     );
 }
 
