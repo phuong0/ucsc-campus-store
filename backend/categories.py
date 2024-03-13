ae58512403f9afa485ac563751b173be162a218a
derekjm2 <derekjm2@yahoo.com>
2024-03-13 16:19:31 -0700

    put categories.py and purchases.py in functions

--- backend/categories.py
+++ backend/categories.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv('UCSC Amazon purchase data Calendar 2023.csv')
+df1 = pd.read_csv('UCSC Amazon purchase data Calendar 2023.csv')
 df2 = pd.read_excel('CruzBuyCY23 Tangible Goods.xlsx')
 
-ret = {}
 
-for column_name in df2.columns:
-    if 'Category' in column_name:
-        ret[column_name] = df2[column_name].dropna().unique()
 
-print(ret)
+
+def categories(df):
+    ret = {}
+    for column_name in df.columns:
+        if 'Category' in column_name:
+            ret[column_name] = df[column_name].dropna().unique()
+    return ret
+
+print(categories(df1))
--- backend/purchases.py
+++ backend/purchases.py
@@ -1,41 +1,28 @@
 import pandas as pd
-df = pd.read_csv('UCSC Amazon purchase data Calendar 2023.csv')
+df1 = pd.read_csv('UCSC Amazon purchase data Calendar 2023.csv')
 df2 = pd.read_excel('CruzBuyCY23 Tangible Goods.xlsx')
 
-ret = {}
-
 specified_categories = ['Beauty', 'Office Product']
-price_column = ''
 
-for column_name in df.columns:
-    if 'Price' in column_name or 'Total' in column_name:
-        price_column = column_name
-        break
-print(price_column)
+def summary(categories, df):
+    ret = {}
+    price_column = ''
 
-for column_name in df.columns:
-    if 'Category' in column_name:
-        categoriesNet = df.groupby(column_name)[price_column].sum()
-        totalCount = df[column_name].value_counts()
-        for cat in specified_categories:
-            x = {}
-            x[price_column] = categoriesNet[cat]
-            x["Count"] = totalCount[cat]
-            ret[cat] = x
-print(ret)
+    for column_name in df.columns:
+        if 'Price' in column_name or 'Total' in column_name:
+            price_column = column_name
+            break
 
+    for column_name in df.columns:
+        if 'Category' in column_name:
+            categoriesNet = df.groupby(column_name)[price_column].sum()
+            totalCount = df[column_name].value_counts()
+            for cat in categories:
+                x = {}
+                x[price_column] = categoriesNet[cat]
+                x["Volume"] = totalCount[cat]
+                ret[cat] = x
+    return ret
 
-categoriesNet = df.groupby('Amazon-Internal Product Category')['Item Net Total'].sum()
-
-print(categoriesNet['Beauty'])
-#
-#category_counts = {category: 0 for category in specified_categories}
-#for category in specified_categories:
-#     category_counts[category] = df['Amazon-Internal Product Category'].value_counts().get(category, 0)
-#
-#print(category_counts)
-#
-#{'Beauty: {Count of Products: , Total Sales: }'}
-
-#for i in x:
+print(summary(specified_categories, df1))
 
