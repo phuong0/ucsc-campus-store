c169c46be0ce50ab1ed33bbc54603ec430b99355
Phuong <phuongunguyen4@gmail.com>
2024-03-20 14:31:04 -0700

    changed website and improved alogorithm

--- frontend/src/Switch.js
+++ frontend/src/Switch.js
@@ -3,6 +3,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import Project from "./pages/Project";
+import File from "./pages/File"
 
 // pages that you can navigate to
 
@@ -15,6 +16,7 @@ export const Switch = () => {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/projects" element={<Project />} />
+                <Route path="/files" element={<File />} />
             </Routes>
         </Router>
     );
