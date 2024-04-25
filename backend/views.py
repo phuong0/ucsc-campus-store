7d87e11d009bc2ae55d016e95a9952f1e9ba77d1
derekjm2 <derekjm2@yahoo.com>
2024-04-25 16:59:22 -0700

    combine full-text and categories in one file

--- backend/views.py
+++ backend/views.py
@@ -3,9 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from django.db import connection
 from django.contrib.auth import authenticate
-from backend.categories import categories
-from backend.purchases import summary
-from backend.full_text import full_text
+from backend.algorithims import categories
+from backend.algorithims import full_text
 import pandas as pd
 
 @csrf_exempt
@@ -144,29 +143,6 @@ def full_text_search(request):
     else:
         return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
 
-#@csrf_exempt 
-#def get_summary(request):
-#    if request.method == 'POST':
-#        try:
-#            file = request.FILES['file']
-#            categories = request.POST.getlist('categories[]')  # Assuming categories are sent as an array
-#            
-#            if file.name.endswith('.csv'):
-#                df = pd.read_csv(file)
-#            elif file.name.endswith('.xlsx'):
-#                df = pd.read_excel(file)
-#            else:
-#                return JsonResponse({'error': 'Unsupported file format'}, status=400)
-#            
-#            # Call the summary function
-#            summary_info = summary(categories, df)
-#            print(summary_info)
-#            return JsonResponse(summary_info, status=200)
-#        
-#        except Exception as e:
-#            return JsonResponse({'error': str(e)}, status=500)
-#    else:
-#        return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
     
 @csrf_exempt
     
